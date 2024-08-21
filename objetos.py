067c651e007bae36513712931ac28510a6f70927
isaniepro1 <isabella.nieto@correounivalle.edu.co>
2024-08-21 10:11:14 -0500

    Add files via upload

---
+++ objetos.py
@@ -0,0 +1,13 @@
+#Ejemplo Laboratorio 1
+#Valery salinas valencia
+#Isabella nieto cardona
+class Auto:
+    marca="chevrolet"
+    modelo=2024 
+    placa="BBQ123"
+
+taxi=Auto()
+print(taxi.modelo)
+print(taxi.marca)
+print(taxi.placa)
+ 
